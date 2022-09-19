284b4c19f3b1d1ae7837bc19a70c230a0e28ff1e
julieyj <julieyjjung@gmail.com>
2022-09-19 03:51:50 -0700

    user books CRUD WIP

---
+++ app/api/user_books_route.py
@@ -0,0 +1,41 @@
+from flask import Blueprint, redirect, request, jsonify
+from flask_login import login_required, current_user
+from app.api.auth_routes import login, validation_errors_to_error_messages
+from app.models import Book, db
+from app.forms import CreateBook
+
+
+user_books_route = Blueprint('user_books', __name__)
+
+@user_books_route.route('/my-books')
+@login_required
+def get_user_books():
+  print('current_user', current_user)
+  user_books = Book.query.filter(Book.user_id == current_user.id).all()
+  user_books_dict = {user_book.id:user_book.to_dict() for user_book in user_books}
+  return user_books_dict
+
+
+@user_books_route.route('/books', methods=['POST'])
+@login_required
+def add_user_book():
+  form = CreateBook()
+  form['csrf_token'].data = request.cookies['csrf_token']
+  data = form.data
+
+  if form.validate_on_submit():
+    book = Book(
+      title = data['title'],
+      year = data['year'],
+      author = data['author'],
+      description = data['description'],
+      image_url = data['image_url'],
+    )
+
+    db.session.add(book)
+    db.session.commit()
+
+    book.to_dict()
+    return redirect('/api/my-books')
+
+  return {'errors': validation_errors_to_error_messages(form.errors)}, 400
---
+++ react-app/src/components/CreateBookModal/index.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Modal } from "../../context/Modal";
+import CreateBookForm from "./CreateBookForm";
+
+function CreateBookModal() {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <div className="create-book-container">
+        <button className="create-book-button" onClick={() => setShowModal(true)}>
+          Add a Book
+        </button>
+        {showModal && (
+          <Modal onClose={() => setShowModal(false)}>
+            <CreateBookForm setShowModal={setShowModal} />
+          </Modal>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default CreateBookModal;
