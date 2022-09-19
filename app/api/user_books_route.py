ba1b1706ee885c497e92372ebbc9f950ca66f0d0
julieyj <julieyjjung@gmail.com>
2022-09-19 15:32:44 -0700

    update user_books_route

--- app/api/user_books_route.py
+++ app/api/user_books_route.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, redirect, request, jsonify
 from flask_login import login_required, current_user
-from app.api.auth_routes import login, validation_errors_to_error_messages
 from app.models import Book, db
 from app.forms import CreateBook
 
@@ -14,28 +13,3 @@ def get_user_books():
   user_books = Book.query.filter(Book.user_id == current_user.id).all()
   user_books_dict = {user_book.id:user_book.to_dict() for user_book in user_books}
   return user_books_dict
-
-
-# @user_books_route.route('/books', methods=['POST'])
-# @login_required
-# def add_user_book():
-#   form = CreateBook()
-#   form['csrf_token'].data = request.cookies['csrf_token']
-#   data = form.data
-
-#   if form.validate_on_submit():
-#     book = Book(
-#       title = data['title'],
-#       year = data['year'],
-#       author = data['author'],
-#       description = data['description'],
-#       image_url = data['image_url'],
-#     )
-
-#     db.session.add(book)
-#     db.session.commit()
-
-#     return book.to_dict()
-#     # return redirect('/api/my-books')
-
-#   return {'errors': validation_errors_to_error_messages(form.errors)}, 400
