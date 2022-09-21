dbb4f63a1e014760c823e93b5539de43133d320c
ben waldee <b1.waldee@gmail.com>
2022-09-21 16:51:22 -0500

    css done for all books page

--- react-app/src/components/BooksListAlex.js
+++ react-app/src/components/BooksListAlex.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { getAllBooksThunk } from '../store/booksAlex';
+import './CSS/BooksListBen.css'
 
 const BooksList = () => {
     console.log('INSIDE BOOKS LIST COMPONENT')
@@ -14,21 +15,22 @@ const BooksList = () => {
         dispatch(getAllBooksThunk())
     }, [dispatch])
 
-    if (booksList.length < 1) return <div>Loading All Books...</div>
+    if (booksList.length < 1) return <div></div>
     return (
-        <>
-            <h1>All Books List: </h1>
-            {booksList.map((book) =>
-                <div key={book.id}>
-                    <NavLink to={`/books/${book.id}`}>
-                    <img src={book.image_url} alt='cover' style={{height: '100px'}}/>
-                    <div>{book.title}</div>
-                    <div>{book.author}</div>
-                    </NavLink>
-                </div>
-
-            )}
-        </>
+        <div className='booklist_outerWrap'>
+            <h1 className='booklist_h1'>Featured Banned Books: </h1>
+            <div className='booklist_outer'>
+                {booksList.map((book) =>
+                    <div className='booklist_bybook' key={book.id}>
+                        <NavLink className='booklist_navLink' to={`/books/${book.id}`}>
+                            <img className='booklist_image' src={book.image_url} alt='cover' />
+                            <div className='booklist_title' >{book.title}</div>
+                            <div className='booklist_author'>{book.author}</div>
+                        </NavLink>
+                    </div>
+                )}
+            </div>
+        </div>
     );
 }
 
