1231becad86bd94ba3461cab690f99c636b03e98
ben waldee <b1.waldee@gmail.com>
2022-09-22 18:28:39 -0500

    merged staging, cleaned up user_books route

--- app/api/user_books_route.py
+++ app/api/user_books_route.py
@@ -1,35 +1,14 @@
 from flask import Blueprint, redirect, request, jsonify
 from flask_login import login_required, current_user
 from app.models import Book, db
-from app.forms import CreateBook
-from sqlalchemy import desc, asc
 
 
 user_books_route = Blueprint('user_books', __name__)
 
-# @user_books_route.route('/my-books')
-# @login_required
-# def get_user_books():
-#   print('current_user', current_user)
-#   user_books = Book.query.filter(Book.user_id == current_user.id).all()
-#   user_books_dict = {user_book.id:user_book.to_dict() for user_book in user_books}
-#   return user_books_dict
-
 @user_books_route.route('/my-books')
 @login_required
 def get_user_books():
-    print('current_user', current_user)
-    user_books_all = Book.query.filter(Book.user_id == current_user.id).all()
-    user_books = user_books_all.order_by(desc(Book.id))
-    user_books_dict = {user_book.id: user_book.to_dict()
-                       for user_book in user_books}
-    return user_books_dict
-
-@user_books_route.route('/my-books')
-@login_required
-def get_user_books():
-    print('current_user', current_user)
-    user_books = Book.query.filter(Book.user_id == current_user.id).order_by(desc(Book.created_at)).all()
-    user_books_dict = {user_book.id: user_book.to_dict()
-                       for user_book in user_books}
-    return user_books_dict
+  print('current_user', current_user)
+  user_books = Book.query.filter(Book.user_id == current_user.id).all()
+  user_books_dict = {user_book.id:user_book.to_dict() for user_book in user_books}
+  return user_books_dict
