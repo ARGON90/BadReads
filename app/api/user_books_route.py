e42f26eae869a334227f51a1aa4fce301df697fb
ben waldee <b1.waldee@gmail.com>
2022-09-23 17:58:41 -0500

    console cleanup

--- app/api/user_books_route.py
+++ app/api/user_books_route.py
@@ -8,7 +8,7 @@ user_books_route = Blueprint('user_books', __name__)
 @user_books_route.route('/my-books')
 @login_required
 def get_user_books():
-  print('current_user', current_user)
+  # print('current_user', current_user)
   user_books = Book.query.filter(Book.user_id == current_user.id).all()
   user_books_dict = {user_book.id:user_book.to_dict() for user_book in user_books}
   return user_books_dict
